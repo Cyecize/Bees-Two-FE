b4137637d195d6f6fe18e529c7fde3780516743d
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-02-03 21:51:39 +0200

    Update BeesParam to support nulls

--- src/app/api/common/bees-param.ts
+++ src/app/api/common/bees-param.ts
@@ -1,14 +1,14 @@
 export interface BeesParam {
   name: string;
-  value?: number | string | boolean;
+  value: number | string | boolean | null;
 }
 
 export class BeesParamImpl implements BeesParam {
   name: string;
-  value?: number | string | boolean;
+  value: number | string | boolean | null;
 
-  constructor(name: string, value?: number | string | boolean) {
+  constructor(name: string, value?: number | string | boolean | null) {
     this.name = name;
-    this.value = value;
+    this.value = value || null;
   }
 }
