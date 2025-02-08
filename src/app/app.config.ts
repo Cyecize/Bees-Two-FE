c11f675416f56b49ad474576e5356ebe187cdafb
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-02-08 08:25:30 +0200

    Add missing config for importing monaco editor.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,10 +10,12 @@ import {
 import { authInterceptor } from './api/auth/auth.interceptor';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    importProvidersFrom(MonacoEditorModule.forRoot()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     importProvidersFrom(MatNativeDateModule),
     importProvidersFrom(BrowserAnimationsModule),
