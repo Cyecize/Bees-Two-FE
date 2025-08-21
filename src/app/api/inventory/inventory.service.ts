336efab1d07f2653524e84b4dea146d513ac83f6
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-08-21 23:28:38 +0300

    Add method for checking whether single item has stock.

--- src/app/api/inventory/inventory.service.ts
+++ src/app/api/inventory/inventory.service.ts
@@ -10,8 +10,14 @@ import { RelayVersion } from '../relay/relay.version';
 import { RelayService } from '../relay/relay.service';
 import { InventoryPayload } from './dto/inventory.payload';
 import { InventoryQuery, InventoryQueryImpl } from './dto/inventory.query';
-import { InventoriesResponse } from './dto/inventories.response';
+import {
+  InventoriesResponse,
+  InventoryResponse,
+} from './dto/inventories.response';
 import { InventoryRepository } from './inventory.repository';
+import { Inventory } from './dto/inventory';
+import { AccountV1Service } from '../accounts/v1/account-v1.service';
+import { PlatformIdService } from '../platformid/platform-id.service';
 
 /**
  * @monaco
@@ -28,6 +34,12 @@ export interface IInventoryService {
     query: InventoryQuery,
     env?: CountryEnvironmentModel,
   ): Promise<WrappedResponse<InventoriesResponse>>;
+
+  getStockForItem(
+    vendorItemId: string,
+    vendorAccountId: string,
+    env: CountryEnvironmentModel,
+  ): Promise<InventoryResponse | null>;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -35,6 +47,8 @@ export class InventoryService implements IInventoryService {
   constructor(
     private relayService: RelayService,
     private repository: InventoryRepository,
+    private accountV1Service: AccountV1Service,
+    private platformIdService: PlatformIdService,
   ) {}
 
   public async addStock(
@@ -64,5 +78,42 @@ export class InventoryService implements IInventoryService {
     return await new FieldErrorWrapper(() =>
       this.repository.searchStock(query, env?.id),
     ).execute();
+  }
+
+  async getStockForItem(
+    vendorItemId: string,
+    vendorAccountId: string,
+    env: CountryEnvironmentModel,
+  ): Promise<InventoryResponse | null> {
+    const query = this.newQuery();
+    const account = await this.accountV1Service.findOne(env, vendorAccountId);
+
+    if (!account) {
+      console.warn(`Invalid account ${vendorAccountId}}!`);
+      return null;
+    }
+
+    const platformId = await this.platformIdService.encodeInventoryId({
+      vendorAccountId: vendorAccountId,
+      vendorItemId: vendorItemId,
+      vendorId: env.vendorId,
+      vendorDeliveryCenterId: account.deliveryCenterId!,
+    });
+
+    query.inventoryPlatformIds.push(platformId.platformId);
+
+    const response = await this.searchStock(query, env);
+
+    if (response.isSuccess) {
+      const inventories = response.response.response.inventories;
+      if (inventories.length > 0) {
+        return inventories[0];
+      }
+
+      return null;
+    }
+
+    console.error(response);
+    return null;
   }
 }
